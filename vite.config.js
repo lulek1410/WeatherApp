5213650a834a076a905ca0e5d1abeb229efb1425
lulek1410 <>
2023-05-16 20:53:17 +0200

    Fix 404

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import reactRefresh from "@vitejs/plugin-react-refresh";
+import historyApiFallback from "connect-history-api-fallback";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), reactRefresh()],
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
 		},
 	},
+	server: {
+		middleware: (options) => {
+			return [historyApiFallback(), options.middleware];
+		},
+	},
+	base: "https://weatherapp-bj6p.onrender.com",
 });
